e4df52e3486c7af80811a91225474537dcaa5a29
Agata <agata@porwit.net>
2021-10-11 13:53:34 -0700

    Initial commit

---
+++ src/agataporwit/Main.java
@@ -0,0 +1,20 @@
+package agataporwit;
+
+import java.util.*;
+
+public class Main {
+    static int solveMeFirst(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+	// write your code here
+
+        int a;
+        a = 5;
+        int b;
+        b = 5;
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+}
